c3dcb71b0dc86801118130cbecf37ef69e893bc7
WhiteChilli <locmerea@gmail.com>
2023-02-14 20:28:54 +0200

    Homework Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        boolean condition = true;
+        //Task 1
+        Scanner userInput = new Scanner(System.in);
 
-        if (condition) {
-            System.out.println( "I have reached this statement" );
-        }
+        System.out.print("Type in a number between 1 and 4 to get a season:  ");
 
-        if ( 1 < 3 ) {
-            System.out.println( "Statements works" );
-        }
+        int month = userInput.nextInt();
 
-        int a = 1;
-        int b = 5;
-
-        if (a < b) {
-            System.out.println( "statement is true" );
-
+        if (month == 1) {
+            System.out.println( "It is summer!" );
+        } else if (month == 2) {
+            System.out.println("It is autumn!");
+        } else if (month == 3) {
+            System.out.println("It is winter!");
+        } else if (month == 4) {
+            System.out.println("It is spring!");
         } else {
-            System.out.println( "statement is false" );
+            System.out.println("Sorry, I do not have a season with this number!");
         }
 
-        // else if
-        // && ||
-        // switch case
-
-        int[] myArray = new int [10];
-        int[] myArrayTwo = {1, 2, 3};
-        myArrayTwo[0] = 2;
-        // myArray.length
-        // myArray [myArray.length -1]
-
-        ArrayList<Integer> array = new ArrayList<Integer>();
-
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
-
-        int x = 7;
-        Integer y = 111;
-        x = y;
-        y = x * 123;
-
+        userInput.close();
 
 
 
