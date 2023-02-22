c9ac4562384953415a66179a580564a4b7e37007
WhiteChilli <locmerea@gmail.com>
2023-02-22 20:37:20 +0200

    Homework updated seasons task

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,20 @@ public class Main {
 
         Scanner userInput = new Scanner(System.in);
 
-        System.out.print("Type in a number between 1 and 4 to get a season:  ");
+        System.out.print("Type in a number of a month between 1 and 12 to get season:  ");
 
         int month = userInput.nextInt();
 
-        if (month == 1) {
-            System.out.println( "It is summer!" );
-        } else if (month == 2) {
-            System.out.println("It is autumn!");
-        } else if (month == 3) {
-            System.out.println("It is winter!");
-        } else if (month == 4) {
+        if (month == 1 || month == 2 || month == 12) {
+            System.out.println( "It is winter!" );
+        } else if (month == 3 || month == 4 || month == 5) {
             System.out.println("It is spring!");
+        } else if (month == 6 || month == 7 || month == 8) {
+            System.out.println("It is summer!");
+        } else if (month == 9 || month == 10 || month == 11) {
+            System.out.println("It is autumn!");
         } else {
-            System.out.println("Sorry, I do not have a season with this number!");
+            System.out.println("Sorry, there is no month with this number");
         }
 
         userInput.close();
